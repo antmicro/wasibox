0575a6517bf977ece90077a9a26916550027a000
Grzegorz Placzek <gplaczek@antmicro.com>
2022-12-01 12:20:02 +0100

    Add unzip

---
+++ src/tools/unzip/src/unzip.rs
@@ -0,0 +1,59 @@
+use std::io;
+use std::fs;
+use std::path::PathBuf;
+
+use clap::{Arg, command};
+
+use zip::ZipArchive;
+
+pub fn unzip() -> io::Result<()> {
+    let matches = command!()
+        .arg(
+            Arg::new("quiet")
+                .help("quiet mode")
+                .short('q')
+                .long("quiet")
+                .num_args(0)
+        )
+        .arg(
+            Arg::new("FILE")
+                .help("Archive to decompress")
+                .required(true)
+                .index(1)
+        ).get_matches();
+    let filepath: PathBuf = PathBuf::from(matches.get_one::<String>("FILE").unwrap());
+    let quiet = matches.contains_id("quiet");
+    if !filepath.is_file() {
+        return Err(io::Error::new(io::ErrorKind::InvalidInput, format!("Cannot find or open {}", filepath.display())));
+    }
+    if let Ok(archive) = &mut ZipArchive::new(fs::File::open(filepath)?) {
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).unwrap();
+            let output_path = file.enclosed_name().to_owned().unwrap();
+            if file.name().ends_with('/') {
+                if !quiet { println!("creating dir {}", output_path.display()); }
+                fs::create_dir_all(&output_path).unwrap();
+                continue;
+            }
+            if let Some(parent) = output_path.parent() {
+                if !parent.exists() {
+                    if !quiet { println!("creating dir {}", parent.display()); }
+                    fs::create_dir_all(&parent)?;
+                }
+            }
+            if !quiet {
+                println!("decompressing {}",
+                    file.enclosed_name().unwrap().display()
+                );
+            }
+            let mut output_file = fs::File::create(&output_path).unwrap();
+            io::copy(&mut file, &mut output_file).unwrap();
+            if !quiet {
+                println!("decompressing {} done.",
+                    file.enclosed_name().unwrap().display()
+                );
+            }
+        }
+    }
+    Ok(())
+}
