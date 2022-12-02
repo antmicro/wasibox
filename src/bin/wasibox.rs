4d7132875f14cdf0e40968aef8ba590b847b40e7
Grzegorz Placzek <gplaczek@antmicro.com>
2022-12-02 15:28:57 +0100

    Set working directory with wasi_ext_lib

--- src/bin/wasibox.rs
+++ src/bin/wasibox.rs
@@ -5,6 +5,11 @@ use wasibox::tools_map::TOOLS_MAP;
 
 fn main() -> io::Result<()> {
     let mut args = env::args();
+    let _ = wasi_ext_lib::chdir(
+        &match wasi_ext_lib::getcwd() {
+            Ok(p) => p,
+            Err(_) => String::from("/")
+        });
     match args.next() {
         Some(s) => {
             if let Some(x) = TOOLS_MAP.get(
