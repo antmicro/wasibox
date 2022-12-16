ea67d8952150d5e385f440ff105b73cc5bb27600
Grzegorz Placzek <gplaczek@antmicro.com>
2022-12-16 13:58:53 +0100

    Optimize `purge`

--- src/tools/purge/src/purge.rs
+++ src/tools/purge/src/purge.rs
@@ -4,16 +4,14 @@ use std::path::PathBuf;
 use std::env::Args;
 use std::collections::HashMap;
 
-fn traverse(path: &PathBuf, paths: &mut Vec<String>) -> io::Result<()>{
+fn traverse(path: &PathBuf, paths: &mut Vec<PathBuf>) -> io::Result<()>{
     if let Ok(a) = fs::read_dir(path) {
         for entry in a {
             let entry = entry?;
-            let path = entry.path();
-            let filename = String::from(path.to_str().unwrap());
             if entry.file_type()?.is_dir() {
                 traverse(&entry.path(), paths)?;
             }
-            paths.push(filename);
+            paths.push(entry.path());
         }
     }
     Ok(())
@@ -28,19 +26,16 @@ pub fn purge(mut _args: Args) -> io::Result<()> {
     let _ = fs::remove_file("/filesystem-initiated");
 
     println!("Starting purge...");
-    let mut files: Vec<String> = vec![];
+    let mut files: Vec<PathBuf> = vec![];
     traverse(&PathBuf::from("/"), &mut files)?;
 
-    for i in files {
-        let path_obj = PathBuf::from(&i);
-        println!("Removing {}", i);
+    for path_obj in files {
+        println!("Removing {}", &path_obj.display());
         if let Err(e) = if path_obj.is_dir() {
-            fs::remove_dir(path_obj)
-        } else {
-            fs::remove_file(path_obj)
-        } {
-            eprintln!("Could not remove {}: {:?}", i, e);
-        }
+                fs::remove_dir(&path_obj)
+            } else {
+                fs::remove_file(&path_obj)
+        } { eprintln!("Could not remove {}: {:?}", &path_obj.display(), e); }
     }
     Ok(())
 }
