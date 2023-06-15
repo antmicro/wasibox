1fc4504c630e2eb1f5c37f51ac07cb87e6a127da
Grzegorz Placzek <gplaczek@antmicro.com>
2023-06-15 14:48:58 +0200

    [#45267] stty: Add size subcommand

--- src/tools/stty/src/stty.rs
+++ src/tools/stty/src/stty.rs
@@ -1,30 +1,42 @@
-use std::io;
 use std::env;
+use std::io;
 
 pub fn stty(args: env::Args) -> io::Result<()> {
-    if let Some(err) = args.map(|arg: String| -> io::Result<()> {
-        let off = arg.starts_with("-");
+    if let Some(err) = args
+        .map(|arg: String| -> io::Result<()> {
+            let mut on = !arg.starts_with('-');
 
-        let arg_: &str = if off {
-            &arg[1..]
-        } else {
-            &arg
-        };
+            let arg_: &str = if on { &arg  } else { &arg[1..] };
 
-        if let Err(errno) = wasi_ext_lib::ioctl(
-            0,
-            match arg_ {
-                "raw" => wasi_ext_lib::IoctlNum::SetRaw,
-                "echo" => wasi_ext_lib::IoctlNum::SetEcho,
-                _ => { return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid argument")); }
-            },
-            Some(&mut (off as i32))
-        ) {
-            Err(io::Error::from_raw_os_error(errno))
-        } else {
-            Ok(())
-        }
-    }).find(|x| { x.is_err() }) {
+            if let Err(errno) = match arg_ {
+                "raw" => wasi_ext_lib::ioctl(0, wasi_ext_lib::IoctlNum::SetRaw, Some(&mut on)),
+                "echo" => wasi_ext_lib::ioctl(0, wasi_ext_lib::IoctlNum::SetRaw, Some(&mut on)),
+                "size" => {
+                    let mut size = [0i32; 2];
+                    let result = wasi_ext_lib::ioctl(
+                        0,
+                        wasi_ext_lib::IoctlNum::GetScreenSize,
+                        Some(&mut size),
+                    );
+                    if let Ok(_) = result {
+                        println!("{} {}", size[0], size[1]);
+                    }
+                    result
+                }
+                _ => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "Invalid argument",
+                    ));
+                }
+            } {
+                Err(io::Error::from_raw_os_error(errno))
+            } else {
+                Ok(())
+            }
+        })
+        .find(|x| x.is_err())
+    {
         err
     } else {
         Ok(())
