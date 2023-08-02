37ddec330a36e201de2fb2e3810f3c8320e5290c
Grzegorz Placzek <gplaczek@antmicro.com>
2023-08-02 11:08:09 +0200

    Add license identifiers

--- src/tools/tar_wasi/src/constants.rs
+++ src/tools/tar_wasi/src/constants.rs
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 pub const GZ_MATCH: &str = "gzip";
 pub const BZ2_MATCH: &str = "bzip2";
 
