ad74b614ab8d196a7e2abc48400c47020bd9dc0a
Grzegorz Placzek <gplaczek@antmicro.com>
2024-07-08 10:00:12 +0200

    [#59970] Add main loop

--- src/tools/init/src/init.rs
+++ src/tools/init/src/init.rs
@@ -4,14 +4,126 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+use std::collections::HashMap;
 use std::env;
+use std::fs;
 use std::io;
+use std::io::{Read, Write};
+
+use wasi_ext_lib::{mknod, spawn, Redirect};
+
+use serde::{Deserialize, Serialize};
 
 mod services;
 
+const FIFO_PATH: &str = "/dev/init.fifo";
+const LOG_PATH: &str = "/tmp/init.log";
+
+#[derive(Deserialize, Serialize)]
+struct SpawnArgs {
+    cmd: String,
+    args: Vec<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+enum Operation {
+    Start(String),
+    Stop(String),
+    Spawn(SpawnArgs),
+}
+
+struct Init {
+    pub(crate) service_manager: services::ServiceManager,
+}
+
+impl Init {
+    fn new() -> Self {
+        Self {
+            service_manager: services::ServiceManager::new(),
+        }
+    }
+
+    fn main_loop(&mut self) -> io::Result<()> {
+        let mut buf = [0u8; 8192];
+
+        mknod(FIFO_PATH, -1).map_err(io::Error::from_raw_os_error)?;
+
+        let mut sock = fs::OpenOptions::new().read(true).open(FIFO_PATH)?;
+        let mut log = fs::OpenOptions::new()
+            .truncate(true)
+            .write(true)
+            .open(LOG_PATH)?;
+
+        loop {
+            let size = sock.read(&mut buf)?;
+            let operation = match serde_json::from_slice(&buf[..size]) {
+                Ok(v) => v,
+                Err(e) => {
+                    _ = log.write(format!("{:?}\n", e).as_bytes())?;
+                    continue;
+                }
+            };
+
+            match operation {
+                Operation::Start(name) => {
+                    let service =
+                        if let Some(service) = self.service_manager.services.get_mut(&name) {
+                            service
+                        } else {
+                            continue;
+                        };
+
+                    if service.pid < 0 {
+                        service.spawn().unwrap();
+                    }
+                }
+                Operation::Stop(name) => {
+                    let service = if let Some(service) = self.service_manager.services.get(&name) {
+                        service
+                    } else {
+                        continue;
+                    };
+
+                    if service.pid > 0 {
+                        wasi_ext_lib::kill(service.pid, wasi::SIGNAL_KILL)
+                            .map_err(io::Error::from_raw_os_error)?;
+                    }
+                }
+                Operation::Spawn(spawn_args) => {
+                    let stdin_path = "/dev/spawn_stdin";
+                    let stdout_path = "/dev/spawn_stdout";
+                    let stderr_path = "/dev/spawn_stderr";
+
+                    mknod(stdin_path, -1).map_err(io::Error::from_raw_os_error)?;
+                    mknod(stdout_path, -1).map_err(io::Error::from_raw_os_error)?;
+                    mknod(stderr_path, -1).map_err(io::Error::from_raw_os_error)?;
+
+                    spawn(
+                        &spawn_args.cmd,
+                        &spawn_args
+                            .args
+                            .iter()
+                            .map(|arg| arg.as_str())
+                            .collect::<Vec<&str>>(),
+                        &HashMap::new(),
+                        true,
+                        &[
+                            Redirect::Read(0, String::from(stdin_path)),
+                            Redirect::Append(1, String::from(stdout_path)),
+                            Redirect::Append(2, String::from(stderr_path)),
+                        ],
+                    )
+                    .map_err(io::Error::from_raw_os_error)?;
+                }
+            }
+        }
+    }
+}
+
 pub fn init(_args: env::Args) -> io::Result<()> {
-    let mut service_manager = services::ServiceManager::new();
-    service_manager.load_services()?;
-    service_manager.spawn_services()?;
+    let mut init = Init::new();
+    init.service_manager.load_services()?;
+    init.service_manager.spawn_services()?;
+    init.main_loop()?;
     Ok(())
 }
--- src/tools/init/src/services.rs
+++ src/tools/init/src/services.rs
@@ -9,26 +9,59 @@ use wasi_ext_lib::{spawn, Redirect};
 
 #[derive(Deserialize, Debug)]
 pub(crate) struct Service {
+    pub(crate) name: String,
     pub(crate) stdin: String,
     pub(crate) stdout: String,
     pub(crate) stderr: String,
     pub(crate) cmd: String,
     pub(crate) args: Vec<String>,
+
+    #[serde(skip_deserializing)]
+    pub(crate) pid: i32,
+}
+
+impl Service {
+    pub fn spawn(&mut self) -> io::Result<()> {
+        let (_, pid) = spawn(
+            &self.cmd,
+            &self
+                .args
+                .iter()
+                .map(|arg| arg.as_str())
+                .collect::<Vec<&str>>(),
+            &HashMap::new(),
+            true,
+            &[
+                Redirect::Read(0, self.stdin.clone()),
+                Redirect::Append(1, self.stdout.clone()),
+                Redirect::Append(2, self.stderr.clone()),
+            ],
+        )
+        .map_err(io::Error::from_raw_os_error)?;
+
+        self.pid = pid;
+        Ok(())
+    }
 }
 
 pub(crate) struct ServiceManager {
-    services: Vec<Service>,
+    pub(crate) services: HashMap<String, Service>,
 }
 
 impl ServiceManager {
     pub fn new() -> Self {
-        Self { services: vec![] }
+        Self {
+            services: HashMap::new(),
+        }
     }
 
     fn read_services() -> io::Result<impl std::iter::Iterator<Item = io::Result<Service>>> {
         Ok(
             fs::read_dir("/etc/init.d")?.map(|entry| -> io::Result<Service> {
-                if let Ok(service) = serde_json::from_reader(fs::File::open(entry?.path())?) {
+                if let Ok(mut service) =
+                    serde_json::from_reader::<fs::File, Service>(fs::File::open(entry?.path())?)
+                {
+                    service.pid = -1;
                     Ok(service)
                 } else {
                     Err(io::Error::new(
@@ -41,33 +74,17 @@ impl ServiceManager {
     }
 
     pub fn load_services(&mut self) -> io::Result<()> {
-        self.services = Self::read_services()?.flatten().collect::<Vec<Service>>();
+        self.services = Self::read_services()?
+            .flatten()
+            .map(|service| (service.name.clone(), service))
+            .collect::<HashMap<String, Service>>();
         Ok(())
     }
 
-    pub fn spawn_services(&self) -> io::Result<()> {
+    pub fn spawn_services(&mut self) -> io::Result<()> {
         self.services
-            .iter()
-            .map(|service| -> io::Result<()> {
-                spawn(
-                    &service.cmd,
-                    &service
-                        .args
-                        .iter()
-                        .map(|arg| arg.as_str())
-                        .collect::<Vec<&str>>(),
-                    &HashMap::new(),
-                    true,
-                    &[
-                        Redirect::Read(0, service.stdin.clone()),
-                        Redirect::Append(1, service.stdout.clone()),
-                        Redirect::Append(2, service.stderr.clone()),
-                    ],
-                )
-                .map_err(io::Error::from_raw_os_error)?;
-
-                Ok(())
-            })
+            .values_mut()
+            .map(|service| service.spawn())
             .filter(|r| r.is_err())
             .for_each(|err| eprintln!("{:?}", err));
         Ok(())
