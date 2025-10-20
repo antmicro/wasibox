ed4fb8ccaa7e1b9b6ecbaac5d03e60247b835cfc
Dawid Kaczmarzyk <dkaczmarzyk@internships.antmicro.com>
2025-10-20 16:48:44 +0200

    [#84950] Add name option flags to uname tool

--- src/tools/uname/src/uname.rs
+++ src/tools/uname/src/uname.rs
@@ -7,14 +7,71 @@
 use std::env::Args;
 use std::io;
 
+use clap::ArgGroup;
 use clap::Parser;
 
 #[derive(Parser)]
 #[command(no_binary_name = true)]
-struct CliArgs {}
+#[command(group(
+    ArgGroup::new("name_type")
+        .args(&["href", "protocol", "host", "port", "pathname", "search", "hash", "origin", "user_agent"])
+        .multiple(false)
+))]
+struct CliArgs {
+    #[arg(short = 'H', long)]
+    href: bool,
 
-pub fn uname(_args: Args) -> io::Result<()> {
-    match wasi_ext_lib::uname() {
+    #[arg(short = 'p', long)]
+    protocol: bool,
+
+    #[arg(short = 't', long)]
+    host: bool,
+
+    #[arg(short = 'P', long)]
+    port: bool,
+
+    #[arg(short = 'n', long)]
+    pathname: bool,
+
+    #[arg(short = 's', long)]
+    search: bool,
+
+    #[arg(short = 'S', long)]
+    hash: bool,
+
+    #[arg(short = 'o', long)]
+    origin: bool,
+
+    #[arg(short = 'u', long)]
+    user_agent: bool,
+}
+
+pub fn uname(args: Args) -> io::Result<()> {
+    let cli = CliArgs::parse_from(args);
+
+    let name_type = if cli.href {
+        wasi_ext_lib::NameType::Href
+    } else if cli.protocol {
+        wasi_ext_lib::NameType::Protocol
+    } else if cli.host {
+        wasi_ext_lib::NameType::Host
+    } else if cli.port {
+        wasi_ext_lib::NameType::Port
+    } else if cli.pathname {
+        wasi_ext_lib::NameType::Pathname
+    } else if cli.search {
+        wasi_ext_lib::NameType::Search
+    } else if cli.hash {
+        wasi_ext_lib::NameType::Hash
+    } else if cli.origin {
+        wasi_ext_lib::NameType::Origin
+    } else if cli.user_agent {
+        wasi_ext_lib::NameType::UserAgent
+    } else {
+        wasi_ext_lib::NameType::Href
+    };
+
+    match wasi_ext_lib::uname(name_type) {
         Ok(info) => {
             println!("{}", info);
             Ok(())
